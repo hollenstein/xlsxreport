2d81f728eec24efe31d6ddcf86cda5353fde3376
David M. Hollenstein <hollenstein.david@gmail.com>
2023-11-07 10:48:41 +0100

    test: Add MaxQuant example integration test for new xlsxreport implementation
    Note that this test is meant to fail until the new implementation of xlsxreport
    is feature complete in regard to the old implementation.

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,9 +1,14 @@
 import os
 
-import pytest
 import pandas as pd
+import pytest
 import openpyxl
+import xlsxwriter
+
 import xlsxreport
+from xlsxreport.excel_writer import TableSectionWriter
+from xlsxreport.template import ReportTemplate
+from xlsxreport.compiler import prepare_table_sections
 
 
 @pytest.fixture()
@@ -18,46 +23,96 @@ def temp_excel_path(request, tmp_path):
     return output_path
 
 
-def test_correct_creation_of_formatted_excel_file(temp_excel_path):
-    config_path = "./tests/testdata/mq_protein_config.yaml"
-    mq_path = "./tests/testdata/mq_proteinGroups.txt"
-    table = pd.read_csv(mq_path, sep="\t")
-    with xlsxreport.Reportbook(temp_excel_path) as reportbook:
-        protein_sheet = reportbook.add_datasheet("Proteins")
-        protein_sheet.apply_configuration(config_path)
-        protein_sheet.add_data(table)
-        protein_sheet.write_data()
+class TestCorrectCreationOfFormattedExcelFile:
+    def test_old_xlsx_report_implementation(self, temp_excel_path):
+        config_path = "./tests/testdata/mq_protein_config.yaml"
+        mq_path = "./tests/testdata/mq_proteinGroups.txt"
+        table = pd.read_csv(mq_path, sep="\t")
+        with xlsxreport.Reportbook(temp_excel_path) as reportbook:
+            protein_sheet = reportbook.add_datasheet("Proteins")
+            protein_sheet.apply_configuration(config_path)
+            protein_sheet.add_data(table)
+            protein_sheet.write_data()
 
-    reference_excel_path = "./tests/testdata/mq_proteinGroups.xlsx"
-    with open(temp_excel_path, "rb") as f1, open(reference_excel_path, "rb") as f2:
-        wb1 = openpyxl.load_workbook(f1)
-        wb2 = openpyxl.load_workbook(f2)
+        reference_excel_path = "./tests/testdata/mq_proteinGroups.xlsx"
+        with open(temp_excel_path, "rb") as f1, open(reference_excel_path, "rb") as f2:
+            wb1 = openpyxl.load_workbook(f1)
+            wb2 = openpyxl.load_workbook(f2)
 
-        # Compare the number of sheets
-        assert len(wb1.sheetnames) == len(wb2.sheetnames)
+            # Compare the number of sheets
+            assert len(wb1.sheetnames) == len(wb2.sheetnames)
 
-        # Compare the sheet names
-        for sheet1, sheet2 in zip(wb1, wb2):
-            assert sheet1.title == sheet2.title
+            # Compare the sheet names
+            for sheet1, sheet2 in zip(wb1, wb2):
+                assert sheet1.title == sheet2.title
 
-        # Compare the cell values and formatting
-        for sheet1, sheet2 in zip(wb1, wb2):
-            for row1, row2 in zip(sheet1.iter_rows(), sheet2.iter_rows()):
-                for cell1, cell2 in zip(row1, row2):
-                    assert cell1.value == cell2.value
-                    assert cell1._style == cell2._style
-                    assert str(cell1.number_format) == str(cell2.number_format)
-                    assert str(cell1.font) == str(cell2.font)
-                    assert str(cell1.border) == str(cell2.border)
-                    assert str(cell1.fill) == str(cell2.fill)
-                    assert str(cell1.alignment) == str(cell2.alignment)
+            # Compare the cell values and formatting
+            for sheet1, sheet2 in zip(wb1, wb2):
+                for row1, row2 in zip(sheet1.iter_rows(), sheet2.iter_rows()):
+                    for cell1, cell2 in zip(row1, row2):
+                        assert cell1.value == cell2.value
+                        assert cell1._style == cell2._style
+                        assert str(cell1.number_format) == str(cell2.number_format)
+                        assert str(cell1.font) == str(cell2.font)
+                        assert str(cell1.border) == str(cell2.border)
+                        assert str(cell1.fill) == str(cell2.fill)
+                        assert str(cell1.alignment) == str(cell2.alignment)
 
-        # Compare the conditional formatting rules
-        for sheet1, sheet2 in zip(wb1, wb2):
-            for rule1, rule2 in zip(
-                sheet1.conditional_formatting._cf_rules,
-                sheet2.conditional_formatting._cf_rules,
-            ):
-                assert rule1.cells == rule2.cells
-                for cfrule1, cfrul2 in zip(rule1.rules, rule2.rules):
-                    assert str(cfrule1) == str(cfrul2)
+            # Compare the conditional formatting rules
+            for sheet1, sheet2 in zip(wb1, wb2):
+                for rule1, rule2 in zip(
+                    sheet1.conditional_formatting._cf_rules,
+                    sheet2.conditional_formatting._cf_rules,
+                ):
+                    assert rule1.cells == rule2.cells
+                    for cfrule1, cfrul2 in zip(rule1.rules, rule2.rules):
+                        assert str(cfrule1) == str(cfrul2)
+
+    def test_new_xlsx_report_implementation(self, temp_excel_path):
+        config_path = "./tests/testdata/mq_protein_config.yaml"
+        mq_path = "./tests/testdata/mq_proteinGroups.txt"
+        # table = pd.read_csv(mq_path, sep="\t")
+        table = pd.read_csv(mq_path, sep="\t").fillna("")
+
+        report_template = ReportTemplate.load(config_path)
+        table_sections = prepare_table_sections(report_template, table)
+        with xlsxwriter.Workbook(temp_excel_path) as workbook:
+            worksheet = workbook.add_worksheet("Proteins")
+            section_writer = TableSectionWriter(workbook)
+            section_writer.write_sections(
+                worksheet, table_sections, settings=report_template.settings
+            )
+
+        reference_excel_path = "./tests/testdata/mq_proteinGroups.xlsx"
+        with open(temp_excel_path, "rb") as f1, open(reference_excel_path, "rb") as f2:
+            wb1 = openpyxl.load_workbook(f1)
+            wb2 = openpyxl.load_workbook(f2)
+
+            # Compare the number of sheets
+            assert len(wb1.sheetnames) == len(wb2.sheetnames)
+
+            # Compare the sheet names
+            for sheet1, sheet2 in zip(wb1, wb2):
+                assert sheet1.title == sheet2.title
+
+            # Compare the cell values and formatting
+            for sheet1, sheet2 in zip(wb1, wb2):
+                for row1, row2 in zip(sheet1.iter_rows(), sheet2.iter_rows()):
+                    for cell1, cell2 in zip(row1, row2):
+                        assert cell1.value == cell2.value
+                        assert cell1._style == cell2._style
+                        assert str(cell1.number_format) == str(cell2.number_format)
+                        assert str(cell1.font) == str(cell2.font)
+                        assert str(cell1.border) == str(cell2.border)
+                        assert str(cell1.fill) == str(cell2.fill)
+                        assert str(cell1.alignment) == str(cell2.alignment)
+
+            # Compare the conditional formatting rules
+            for sheet1, sheet2 in zip(wb1, wb2):
+                for rule1, rule2 in zip(
+                    sheet1.conditional_formatting._cf_rules,
+                    sheet2.conditional_formatting._cf_rules,
+                ):
+                    assert rule1.cells == rule2.cells
+                    for cfrule1, cfrul2 in zip(rule1.rules, rule2.rules):
+                        assert str(cfrule1) == str(cfrul2)
