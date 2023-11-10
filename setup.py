49d3edae38d54d6f1f4efe61a45366bfc7c1082c
David M. Hollenstein <hollenstein.david@gmail.com>
2023-11-10 10:39:35 +0100

    chore: Change build config file to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,27 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-VERSION = "0.0.7"
-
-packages = find_packages()
-packages.append("xlsxreport.default_config")
-
-setup(
-    name="xlsxreport",
-    version=VERSION,
-    license="Apache v2",
-    author="David M. Hollenstein",
-    author_email="hollenstein.david@gmail.com",
-    install_requires=["numpy", "pandas", "pyyaml", "xlsxwriter", "click", "appdirs"],
-    python_requires=">=3.9",
-    packages=packages,
-    package_data={"xlsxreport.default_config": ["*.yaml"]},
-    entry_points={
-        "console_scripts": [
-            "xlsxreport_setup = xlsxreport.scripts.setup_appdata_dir:cli",
-            "xlsxreport = xlsxreport.scripts.report:cli",
-            "cassiopeia_report = xlsxreport.scripts.cassiopeia_report:cli",
-        ],
-    },
-    keywords=["excel", "report", "mass spectrometry", "proteomics"],
-)
+setup()
