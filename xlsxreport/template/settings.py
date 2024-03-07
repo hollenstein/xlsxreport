a6efc1ac419d367b39b8e8e1901af85e82f2a6e6
David M. Hollenstein <hollenstein.david@gmail.com>
2024-03-07 16:35:47 +0100

    fix: Make ReportTemplateSettings.schema immutable

--- xlsxreport/template/settings.py
+++ xlsxreport/template/settings.py
@@ -12,33 +12,37 @@ class ReportTemplateSettings(UserDict):
 
     def __init__(self, data: dict):
         self._validator = cerberus.Validator(require_all=False, allow_unknown=False)
-        self.schema = SETTINGS_SCHEMA
+        self._schema = SETTINGS_SCHEMA
 
-        settings = {key: value for key, value in data.items() if key in self.schema}
-        if not self._validator.validate(settings, self.schema):
+        settings = {key: value for key, value in data.items() if key in self._schema}
+        if not self._validator.validate(settings, self._schema):
             raise TypeError(f"Invalid settings: {self._validator.errors}")
 
         self.data: dict = settings
 
     def __getitem__(self, key: str) -> str | float | bool:
-        if key not in self.schema:
+        if key not in self._schema:
             raise KeyError(f"Invalid setting argument: {key}")
 
         if key in self.data:
             return self.data[key]
         else:
-            return self.schema[key]["default"]
+            return self._schema[key]["default"]
 
     def __repr__(self):
-        length = max([len(key) for key in self.schema])
+        length = max([len(key) for key in self._schema])
 
         output = []
-        for parameter in self.schema:
+        for parameter in self._schema:
             value = _format_value(self.get(parameter), quote_char='"')
             if parameter not in self.data:
                 value = f"{value} (default)"
             output.append(f"{parameter:<{length}} : {value}")
         return "\n".join(output)
+
+    @property
+    def schema(self):
+        return deepcopy(self._schema)
 
     def to_dict(self) -> dict:
         """Return a copy of the settings as a dictionary."""
