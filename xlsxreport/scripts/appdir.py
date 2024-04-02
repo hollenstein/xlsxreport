7b8dc6bf3ffaebcf12f3c742cb01d8c400ef981e
David M. Hollenstein <hollenstein.david@gmail.com>
2024-04-02 15:50:58 +0200

    feat: Add option to list templates in the appdir, minor fixes in option rules

--- xlsxreport/scripts/appdir.py
+++ xlsxreport/scripts/appdir.py
@@ -14,6 +14,7 @@ HELP = {
         "with the '--setup' option."
     ),
     "reveal": "Reveal the app directory in the file explorer.",
+    "templates": "List the table template files in the app directory.",
 }
 
 
@@ -21,31 +22,37 @@ HELP = {
 @click.option("--setup", is_flag=True, default=False, help=HELP["setup"])
 @click.option("--overwrite", is_flag=True, default=False, help=HELP["overwrite"])
 @click.option("-r", "--reveal", is_flag=True, default=False, help=HELP["reveal"])
-def appdir_command(setup, overwrite, reveal) -> None:
+@click.option("-t", "--templates", is_flag=True, default=False, help=HELP["reveal"])
+def appdir_command(setup, overwrite, reveal, templates) -> None:
     """Locate app directory, optionally create the directory and copy default table
     template files."""
     appdir = xlsxreport.appdir.locate_appdir()
-    if not setup:
-        if os.path.isdir(appdir):
-            click.echo(appdir)
-        else:
-            click.echo(
-                "XlsxReport app directory not found, run `xlsxreport appdir --setup` "
-                "to create the app directory."
-            )
     if setup:
         if not os.path.isdir(appdir):
-            click.echo(f"Creating XlsxReport app directory at: {appdir}")
+            click.echo(f"Creating XlsxReport app directory at:\n  {appdir}")
         else:
-            click.echo(f"XlsxReport app directory found at: {appdir}")
+            click.echo(f"XlsxReport app directory found at:\n  {appdir}")
 
         if overwrite:
             click.echo(
-                "Copying default table templates to the app directory, overwriting "
-                "existing files."
+                "  Copying default table templates to the app directory, overwriting "
+                "existing files"
             )
         else:
-            click.echo("Copying missing default table templates to the app directory.")
+            click.echo("  Copying missing default table templates to the app directory")
         xlsxreport.appdir.setup_appdir(overwrite_templates=overwrite)
+    elif not os.path.isdir(appdir):
+        click.echo(
+            "XlsxReport app directory not found, run `xlsxreport appdir --setup` "
+            "to create the app directory."
+        )
+        return
+
+    if not any([setup, reveal, templates]):
+        click.echo(appdir)
     if reveal:
         click.launch(appdir)
+    if templates:
+        click.echo("Table template files in the app directory:")
+        for template in xlsxreport.appdir.get_appdir_templates():
+            click.echo(f"  {template}")
